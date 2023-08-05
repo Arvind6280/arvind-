3591df04d7b3cbc39b6fc243db242d2b5e8e81d5
Arvind6280 <121429131+Arvind6280@users.noreply.github.com>
2023-08-05 12:56:52 +0530

    c++

---
+++ Untitled8.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	int m[3][5],i,j;
+	for(i=0;i<=2;i++){
+		printf("enter all 5 subject marks of %d student:",i+1);
+		for(j=0;j<=4;j++){
+			
+		}
+		scanf("%d",&m[i][j]);
+	}
+}
