cc9ec48c30482777d6ff154045798a7f5a7d0c07
Arvind6280 <121429131+Arvind6280@users.noreply.github.com>
2023-08-05 12:53:59 +0530

    c++

---
+++ Untitled3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(){
+	int a[3][2]={{10,20},{50,54},{78,80}},i;
+	printf("%d\t %d\n,a[2][1],a[0][1]");
+		printf("%d\t %d\n,a[2][1],a[0][1]");
+			printf("%d\t %d\n,a[2][1],a[0][1]");
+			for(i=0;i<=2;i++){
+				printf("%d\t %d\t ,a[2][1],a[0][1]");
+			}
+}
