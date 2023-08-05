ad60bf6333555a7685ba4281098afd1d6377048f
Arvind6280 <121429131+Arvind6280@users.noreply.github.com>
2023-08-05 12:56:13 +0530

    c++

---
+++ Untitled6.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	int a[4][3]={{2,3,4},{8,7,8},{8,6,5},{9,8,7}},i,j;
+	for(i=0;i<=3;i++){
+		for(j=0;j<=2;j++){
+			
+		}
+			printf("%d\t %d\t ",a[i][j]);
+	
+	}
+printf("\n");
+	
+}
